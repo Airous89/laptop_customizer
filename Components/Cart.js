685bc9bedffcd2cc82889b73359f9b5896c2002d
Airous89 <50056994+Airous89@users.noreply.github.com>
2019-09-20 16:56:19 -0400

    Add files via upload

---
+++ Components/Cart.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import app from '../App.css/'
+
+
+class Cart extends Component{
+  render (){
+    const Cart = Object.keys(this.props.selectedOption)
+    .reduce((acc, curr) => acc + this.state.selected[curr].cost,0);
+    const TotalCost = Object.keys(this.props.selected)
+    .map(key =>  <div className="summary__option" key={key}>
+      <div className="summary__option__label">{key}</div>
+      <div className="summary__option__value">{this.props.selected[key].name}</div>
+      <div className="summary__option__cost">
+        {new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD'}).format(this.props.selected[key].cost)}
+      </div>
+    </div>)
+    return(
+      <div>
+        <section className="Your cart">
+          <h3>Your Cart</h3>
+            {TotalCost}
+          <div className="summary__total"></div>
+          <div className="summary__total">Total Price: </div>
+          <div className="summary__total__value">
+            {new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD'}).format(Cart)}
+
+          </div>
+        </section>
+      </div>
+    )
+  }
+}
+
+
+export default Cart;
