67aca3116241841de14bd1243493296a3a8f274c
Airous89 <50056994+Airous89@users.noreply.github.com>
2019-09-20 18:33:37 -0400

    Add files via upload

---
+++ src/Components/Head.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+
+class Head extends Component {
+    render() {
+      return (
+        <header className="header">
+        <h1>ELF Computing | Laptops</h1>
+        <h2>Customize your laptop</h2>
+        <section className="main__summary">
+          <h2>Your cart</h2>  
+          </section>
+        </header>   
+      )
+    }
+}
+
+export default Head;
